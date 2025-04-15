119d6445990e6685cfc56f5f68aafa71548851df
Devops <109498305+projectaws741@users.noreply.github.com>
2025-04-16 01:04:22 +0530

    Update app.py

--- db-app/app.py
+++ db-app/app.py
@@ -104,7 +104,7 @@ def submit():
         finally:
             connection.close()
     else:
-        return "Failed to connect to the database"
+        return "Failed to connect to the database because database is not up"
 
 @app.route('/getdata')
 def get_data():
